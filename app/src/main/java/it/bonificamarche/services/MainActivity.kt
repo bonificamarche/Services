8fef91898acafff3d37cb9783c16045fd7d92fe2
lollo_pagno <lorenzo.pagnini@yahoo.it>
2022-02-28 10:48:42 +0100

    Start service by activity and completed binding with the client

---
+++ app/src/main/java/it/bonificamarche/services/MainActivity.kt
@@ -0,0 +1,16 @@
+package it.bonificamarche.services
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import it.bonificamarche.services.services.RemoteService
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val service = startService(Intent(this, RemoteService::class.java))
+    }
+}
--- app/src/main/java/it/bonificamarche/services/services/RemoteService.kt
+++ app/src/main/java/it/bonificamarche/services/services/RemoteService.kt
@@ -4,6 +4,7 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.os.Process
+import android.util.Log
 import it.bonificamarche.services.IRemoteService
 
 class RemoteService : Service() {
@@ -12,10 +13,17 @@ class RemoteService : Service() {
         return binder
     }
 
+    override fun onCreate() {
+        super.onCreate()
+        show("Service created")
+    }
+
     private val binder = object : IRemoteService.Stub() {
 
         override fun getPid(): Int {
-            return Process.myPid()
+            val pid = Process.myPid()
+            show("*** New call: pid is $pid")
+            return pid
         }
 
         override fun basicTypes(
@@ -25,6 +33,14 @@ class RemoteService : Service() {
             aFloat: Float,
             aDouble: Double,
             aString: String
-        ) { }
+        ) {
+        }
+    }
+
+    companion object {
+        const val TAG = "RemoteService"
+        val show = { log: String ->
+            Log.e(TAG, log)
+        }
     }
 }
